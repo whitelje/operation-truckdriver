98b27b2297687243efef81205fd9dbae0226678f
Jake Whiteley <whitelje@rose-hulman.edu>
2016-02-16 17:19:40 -0500

    J1939 Message Parsing for Required PGNs

--- OperationTruckdriver/app/src/main/java/edu/rosehulman/beyerpc_whitelje/operationtruckdriver/Constants.java
+++ OperationTruckdriver/app/src/main/java/edu/rosehulman/beyerpc_whitelje/operationtruckdriver/Constants.java
@@ -10,13 +10,27 @@ public class Constants {
     public static final int MESSAGE_WRITE = 3;
     public static final int MESSAGE_DEVICE_NAME = 4;
     public static final int MESSAGE_TOAST = 5;
+    public static final int MESSAGE_STATS_OBD = 6;
+    public static final int MESSAGE_STATS_J1939 = 7;
+    public static final int MESSAGE_RX_OBD = 8;
+    public static final int MESSAGE_RX_J1939 = 9;
+
+
+    public static final int MAX_MESSAGE_SIZE = 250;
 
     public static final String DEVICE_NAME = "device_name";
     public static final String TOAST = "toast";
-    public static final int MAX_MESSAGE_SIZE = 250;
-    public static final int MESSAGE_RX_OBD = 8;
     public static final String OBD_PID = "obd_pid";
     public static final String OBD_DATA = "obd_data";
-    public static final int MESSAGE_STATS_OBD = 6;
     public static final String STATS_COUNT = "stats_count";
+    public static final String J1939_PGN = "j1939_pgn";
+    public static final String J1939_VALUE = "j1939_value";
+    public static final String J1939_SPN = "j1939_spn";
+    public static final String J1939_FMI = "j1939_fmi";
+    public static final String J1939_OC = "j1939_oc";
+    public static final String J1939_ACTIVE = "j1939_active";
+    public static final String J1939_MAKE = "j1939_make";
+    public static final String J1939_MODEL = "j1939_model";
+    public static final String J1939_SERIAL = "j1939_serial";
+    public static final String J1939_VIN = "j1939_vin";
 }
